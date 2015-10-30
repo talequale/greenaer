c7e9ad1039dd4b28ad17fccb887fd36f2304b0a2
Юлия Антонова <antonova.julia@gmail.com>
2015-10-30 19:27:22 +0300

    база + примерная разметка

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+$(function() {
+
+  // скролл до якоря
+  $(document).ready(function(){
+    $("a[href*=#]").bind("click", function(e){
+      var anchor = $(this);
+      $("html, body").stop().animate({
+        scrollTop: $(anchor.attr("href")).offset().top
+      }, 1000);
+        e.preventDefault();
+    });
+    return false;
+  });
+});
